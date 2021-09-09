da0d51ce77fda36c74cba80e8171bdc4b7f3c07c
ekfaktldk11 <ekfaktldk11@naver.com>
2021-09-09 22:11:35 +0900

    erase remark from fetchData.js

--- temp/fetchData.js
+++ temp/fetchData.js
@@ -53,12 +53,10 @@ export const fetchSafety = async (nation, iso, pgNum) => {
     };
 
     const resData = await response.json();
-    //console.log(resData);
 
     if (resData.currentCount > 0) {
         dataExist = true;
         for (const index in resData.data) {
-            //console.log(resData.data[index]);
             loadedData.push(new Safety(
                 resData.data[index].country_nm,
                 resData.data[index].sfty_notice_id,
@@ -66,8 +64,6 @@ export const fetchSafety = async (nation, iso, pgNum) => {
                 resData.data[index].txt_origin_cn,
                 resData.data[index].wrt_dt
             ))
-            //console.log(resData.data[index].country_nm);
-            //console.log(loadedData);
         }
     }
     if (dataExist) return loadedData;
@@ -133,21 +129,6 @@ export const fetchDailyTotal = async (iso) => {
     });
     return res
 }
-
-// // 지역별 총 확진, 격리해제, 사망
-// export const fetchRegionalByCountry = async (iso) => {
-//     let baseUrl = `https://covid-19-global-tracker-with-regional-data.p.rapidapi.com/api/covid/regionalDataByCountry/${iso}`
-//     const res = await fetch(baseUrl, {
-//         method: 'GET',
-//         headers: {
-//             "x-authorization": "6179002e-6646-4852-be37-572758a58cbb",
-//             "x-rapidapi-key": "27b47f90bamsh0076c9c039efb2ep1ea28fjsn79937e73c16d",
-//             "x-rapidapi-host": "covid-19-global-tracker-with-regional-data.p.rapidapi.com",
-//             "useQueryString": true
-//         }
-//     });
-//     return res
-// }
 
 export const fetchEntryPolicy = async () => {
     let DOMParser = require('xmldom').DOMParser;
